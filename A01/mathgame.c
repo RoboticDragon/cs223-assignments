7c39adc21565fd4217535756292a46f90ff979fd
RoboticDragon <hilaryrlutz@gmail.com>
2023-01-23 15:38:15 -0500

    Update mathgame.c

--- A01/mathgame.c
+++ A01/mathgame.c
@@ -7,5 +7,24 @@
 #include <stdio.h>
 
 int main() {
+  int numTimes = 0;
+  int numWon = 0;
+  int result = 0;
+  printf("Welcome to this math game! \n");
+  printf("How many times do you want to play? \n");
+  scanf("%d", numTimes);
+  for(int i = 0; i < numTimes; i ++){
+    int one = rand() % 10;
+    int two = rand() % 10;
+    printf("What is %d", one, " + %d", two, "? ");
+    scanf("%d", result);
+    if(result == (one + two)){
+      numWon ++;
+      printf("Correct!\n")
+    } else {
+     printf("Incorrect!\n") 
+    }
+  }
+  printf("You got %d", numWon, " out of %d", numTimes, " correct. Good game!");
   return 0;
 }
