37e5f0f6074c0380266a71446b024f143c2fb6f6
Hilary R Lutz <hlutz@brynmawr.edu>
2023-02-09 16:40:11 -0500

    assignment 3 half complete

--- A03/dynamic_songs.c
+++ A03/dynamic_songs.c
@@ -1,10 +1,35 @@
 /*----------------------------------------------
- * Author: 
- * Date: 
- * Description
+ * Author: Hilary Lutz
+ * Date: 2/8/23
+ * Description: A program that reads in songs from a csv file. 
  ---------------------------------------------*/
 #include <stdio.h>
-
+#include <string.h>
+#include <stdbool.h>
+struct Song{
+  char title[128];
+  char artist[128];
+  int durSec;
+  int durMin;
+  float dance;
+  float energy;
+  float tempo;
+  float valence;
+};
 int main() {
+    FILE *fp;
+   char str[128];
+
+   /* opening file for reading */
+   fp = fopen("songlist.csv" , "r");
+   if(fp == NULL) {
+      perror("Error opening file");
+      return(-1);
+   }
+   if( fgets (str, 60, fp)!=NULL ) {
+      /* writing content to stdout */
+      puts(str);
+   }
+   fclose(fp);
   return 0;
 }
--- A03/slow.c
+++ A03/slow.c
@@ -1,10 +1,36 @@
 /*----------------------------------------------
- * Author: 
- * Date: 
- * Description
+ * Author: Hilary Lutz
+ * Date: 2/8/23
+ * Description: A program which adds dots between the letters of a phrase. 
  ---------------------------------------------*/
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
 
 int main() {
+  int pause;
+  char word1[32];
+  char *word2; 
+  printf("Pause length: ");
+  scanf(" %d%*c", &pause);
+  printf("Text: ");
+  scanf("%[^\n]%*c", word1);
+  long len = strlen(word1);
+  word2 = malloc(sizeof(char) * (pause + 1) * strlen(word1));
+  printf("sizeof word1: %lu\n", len);
+  printf("sizeof word2: %lu\n", sizeof(word2));
+  printf("sizeof pause: %lu\n", sizeof(pause));
+  printf("word1 * pause: %lu\n", (len * (pause + 1)));
+  for(int i = 0; i < ((pause + 1) * strlen(word1)); i++){
+    if(i % (pause + 1) == 0){
+      word2[i] = word1[i / (pause + 1)];
+    } else{
+      word2[i] = '.';
+    }
+  }
+  word2[strlen(word2)] = '\0';
+  printf("%s \n", word2);
+  free(word2);
   return 0;
 }
