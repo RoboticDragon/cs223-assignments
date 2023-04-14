8acc8ab54b76ba11e9588852c07d5787d002ce09
Hilary R Lutz <hlutz@brynmawr.edu>
2023-04-13 23:42:19 -0400

    assignment almost complete

--- A09/buddhabrot.c
+++ A09/buddhabrot.c
@@ -1,38 +1,325 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <assert.h>
 #include <time.h>
-#include <pthread.h>
+#include <sys/time.h>
 #include "read_ppm.h"
 #include "write_ppm.h"
+#include <assert.h>
+#include <sys/wait.h>
+#include <sys/shm.h>
+#include <sys/ipc.h>
+#include <pthread.h>
+#include <math.h>
+struct thread_data {
+  struct ppm_pixel* pixels; 
+  struct ppm_pixel* palette;
+  int sizex;
+  int sizey; 
+  int startx;
+  int starty; 
+  int xmin; 
+  int ymin; 
+  int xmax; 
+  int ymax;  
+  int maxIterations;
+  pthread_mutex_t lock;
+};
+void *calc(void *userdata){
+  struct thread_data *data = (struct thread_data *) userdata;
+  for(int i = data->startx; i < data->sizex; i++){
+    for(int j = data->starty; j < data->sizey; j++){
+      float xfrac = i / (float)data->sizex;
+      float yfrac = j / (float)data->sizey;
+      float x0 = data->xmin + xfrac * (data->xmax - data->xmin);
+      float y0 = data->ymin + yfrac * (data->ymax - data->ymin);
+      float x = 0;
+      float y = 0;
+      int iter = 0;
+      while (iter < data->maxIterations && x*x + y*y < 2*2){
+        float xtmp = x*x - y*y + x0;
+        y = 2*x*y + y0;
+        x = xtmp;
+        iter++;
+      }
+      if (iter < data->maxIterations){
+        data->pixels[j * data->sizey + i].red = data->palette[iter].red;
+        data->pixels[j * data->sizey + i].green = data->palette[iter].green;
+        data->pixels[j * data->sizey + i].blue = data->palette[iter].blue;
+      } // escaped
+      else{
+        data->pixels[j * data->sizex + i].red = 0;
+        data->pixels[j * data->sizex + i].green = 0;
+        data->pixels[j * data->sizex + i].blue = 0;
+      }
+        
+    }
+  }
+  int maxCount = 0;
+   int **memValue = malloc(sizeof(int) * data[0].sizex * data[0].sizey);
+  for(int i = data->startx; i < data->sizex; i++){
+    for(int j = data->starty; j < data->sizey; j++){
+      float xfrac = i / data->sizex;
+      float yfrac = j / data->sizey;
+      float x0 = data->xmin + xfrac * (data->xmax - data->xmin);
+      float y0 = data->ymin + yfrac * (data->ymax - data->ymin);
+
+      int x = 0;
+      int y = 0;
+      int iter = 0;
+      while (iter < data->maxIterations && x*x + y*y < 2*2){
+        int xtmp = x*x - y*y + x0;
+        int y = 2*x*y + y0;
+        x = xtmp;
+        iter++;
+      }
+        
+
+      if (iter < data->maxIterations){
+        memValue[i][j] = 0;
+      } // escaped
+       
+      else{
+        memValue[i][j] = 1;
+      }
+    }
+  }
+  for(int r = 0; r < sizeof(memValue[0]); r++){
+    for(int c = 0; c < sizeof(memValue); c++){
+      if(memValue[r][c] >= 1){
+       int xfrac = r / data->sizex;
+      int yfrac = c / data->sizey;
+      int x0 = data->xmin + xfrac * (data->xmax - data->xmin); 
+      int y0 = data->ymin + yfrac * (data->ymax - data->ymin);
+
+      int x = 0;
+      int y = 0;
+      while (x*x + y*y < 2*2){
+        int xtmp = x*x - y*y + x0;
+        y = 2*x*y + y0;
+        x = xtmp;
+      }
+        
+
+        int yrow = round(data->sizey * (y - data->ymin)/(data->ymax - data->ymin));
+        int xcol = round(data->sizex * (x - data->xmin)/(data->xmax - data->xmin));
+        if (yrow < 0 || yrow >= data->sizey) continue; // out of range
+        if (xcol < 0 || xcol >= data->sizex) continue; // out of range
+
+        memValue[xcol][yrow] ++;
+        maxCount++;
+      }
+    }
+  }
+  pthread_mutex_lock(&(data->lock));
+  float gamma = 0.681;
+ float factor = 1.0/gamma;
+for(int i = 0; i < sizeof(memValue[0]); i++){
+  for(int j = 0; j < sizeof(memValue); j++){
+    int value = 0;
+
+      if(memValue[i][j] > 0){
+        value = log(memValue[i][j]) / log(maxCount);
+        value = pow(value, factor);
+      }
+        
+
+      data->pixels[i + j].red = value * 255;
+      data->pixels[i + j].green = value * 255;
+      data->pixels[i + j].blue = value * 255;
+
+      
+  }
+ 
+}
+ return NULL;
+}
+// void *checkMem(void *userdata){
+//   int maxCount = 0;
+//   struct thread_data *data = (struct thread_data *) userdata;
+//   int **memValue = malloc(sizeof(int) * data[0].sizex * data[0].sizey);
+//   for(int i = data->startx; i < data->sizex; i++){
+//     for(int j = data->starty; j < data->sizey; j++){
+//       float xfrac = i / data->sizex;
+//       float yfrac = j / data->sizey;
+//       float x0 = data->xmin + xfrac * (data->xmax - data->xmin);
+//       float y0 = data->ymin + yfrac * (data->ymax - data->ymin);
+
+//       int x = 0;
+//       int y = 0;
+//       int iter = 0;
+//       while (iter < data->maxIterations && x*x + y*y < 2*2){
+//         int xtmp = x*x - y*y + x0;
+//         int y = 2*x*y + y0;
+//         x = xtmp;
+//         iter++;
+//       }
+        
+
+//       if (iter < data->maxIterations){
+//         memValue[i][j] = 0;
+//       } // escaped
+       
+//       else{
+//         memValue[i][j] = 1;
+//       }
+//     }
+//   }
+//   for(int r = 0; r < sizeof(memValue[0]); r++){
+//     for(int c = 0; c < sizeof(memValue); c++){
+//       if(memValue[r][c] >= 1){
+//        int xfrac = r / data->sizex;
+//       int yfrac = c / data->sizey;
+//       int x0 = data->xmin + xfrac * (data->xmax - data->xmin); 
+//       int y0 = data->ymin + yfrac * (data->ymax - data->ymin);
+
+//       int x = 0;
+//       int y = 0;
+//       while (x*x + y*y < 2*2){
+//         int xtmp = x*x - y*y + x0;
+//         y = 2*x*y + y0;
+//         x = xtmp;
+//       }
+        
+
+//         int yrow = round(data->sizey * (y - data->ymin)/(data->ymax - data->ymin));
+//         int xcol = round(data->sizex * (x - data->xmin)/(data->xmax - data->xmin));
+//         if (yrow < 0 || yrow >= data->sizey) continue; // out of range
+//         if (xcol < 0 || xcol >= data->sizex) continue; // out of range
+
+//         memValue[xcol][yrow] ++;
+//         maxCount++;
+//       }
+//     }
+//   }
+//   pthread_mutex_lock(&(data->lock));
+//   float gamma = 0.681;
+// float factor = 1.0/gamma;
+// for(int i = 0; i < sizeof(memValue[0]); i++){
+//   for(int j = 0; j < sizeof(memValue); j++){
+//     int value = 0;
+
+//       if(memValue[i][j] > 0){
+//         value = log(memValue[i][j]) / log(maxCount);
+//         value = pow(value, factor);
+//       }
+        
+
+//       data->pixels[i + j].red = value * 255;
+//       data->pixels[i + j].green = value * 255;
+//       data->pixels[i + j].blue = value * 255;
+
+      
+//   }
+// }
+      
+//   return NULL;
+// }
 
 int main(int argc, char* argv[]) {
+  struct timeval stop, start;
+  double timer = time(0);
+  gettimeofday(&start, NULL);
   int size = 480;
   float xmin = -2.0;
   float xmax = 0.47;
   float ymin = -1.12;
   float ymax = 1.12;
   int maxIterations = 1000;
-  int numProcesses = 4;
 
   int opt;
-  while ((opt = getopt(argc, argv, ":s:l:r:t:b:p:")) != -1) {
+  while ((opt = getopt(argc, argv, ":s:l:r:t:b:")) != -1) {
     switch (opt) {
       case 's': size = atoi(optarg); break;
       case 'l': xmin = atof(optarg); break;
       case 'r': xmax = atof(optarg); break;
       case 't': ymax = atof(optarg); break;
       case 'b': ymin = atof(optarg); break;
-      case '?': printf("usage: %s -s <size> -l <xmin> -r <xmax> "
-        "-b <ymin> -t <ymax> -p <numProcesses>\n", argv[0]); break;
+      case '?': printf("usage: %s -s <size> -l <xmin> -r <xmax> -b <ymin> -t <ymax>\n", argv[0]); break;
     }
   }
+  
   printf("Generating mandelbrot with size %dx%d\n", size, size);
-  printf("  Num processes = %d\n", numProcesses);
   printf("  X range = [%.4f,%.4f]\n", xmin, xmax);
   printf("  Y range = [%.4f,%.4f]\n", ymin, ymax);
+  struct ppm_pixel* palette  = malloc(sizeof(struct ppm_pixel) * maxIterations);
+  struct ppm_pixel* pixels  = malloc(sizeof(struct ppm_pixel) * size * size);
+  for(int x = 0; x <  maxIterations; x++){
+    int basered = rand() % 255;
+    int basegreen = rand() % 255;
+    int baseblue = rand() % 255;
+    palette[x].red = basered + rand() % 100 - 50;
+    palette[x].green = basegreen + rand() % 100 - 50;
+    palette[x].blue = baseblue + rand() % 100 - 50;
+  }
+ 
 
-  // todo: your code here
+  int shmid;
+  shmid = shmget(IPC_PRIVATE, sizeof(char) * size, 0644 | IPC_CREAT);
+  if (shmid == -1) {
+    perror("Error: cannot initialize shared memory\n");
+    exit(1);
+  }
+
+  char* buffer = shmat(shmid, NULL, 0);
+  if (buffer == (void*) -1) {
+    perror("Error: cannot access shared memory\n");
+    exit(1);
+  } 
+  pthread_t threads[4];
+  struct thread_data data[4];
+  for (int i = 0; i < 4; i++) {
+    data[i].maxIterations = maxIterations;
+    data[i].pixels = pixels;
+    data[i].palette = palette;
+    data[i].xmax = xmax;
+    data[i].xmin = xmin;
+    data[i].ymax = ymax;
+    data[i].ymin = ymin;
+    if(i == 0){
+      data[i].startx = 0;
+      data[i].starty = 0;
+      data[i].sizex = size/2;
+      data[i].sizey = size / 2;
+    } else if(i == 1){
+      data[i].startx = 0;
+      data[i].starty = size / 2;
+      data[i].sizex = size / 2;
+      data[i].sizey = size;
+    } else if(i == 2){
+      data[i].startx = size/2;
+      data[i].sizex = size;
+      data[i].starty = 0;
+      data[i].sizey = size /2;
+    } else {
+      data[i].startx = size/2;
+      data[i].sizex = size;
+      data[i].starty = size/2;
+      data[i].sizey = size;
+    }
+    pthread_create(&threads[i], NULL, calc, (void*) &data[i]);
+  }
+  gettimeofday(&stop, NULL);
+  timer = stop.tv_sec - start.tv_sec + (stop.tv_usec - start.tv_usec)/1.e6;
+  printf("time is %f\n",timer);
+
+  if (shmdt(buffer) == -1) {
+    perror("Error: cannot detatch from shared memory\n");
+    exit(1);
+  }
+
+  if (shmctl(shmid, IPC_RMID, 0) == -1) {
+    perror("Error: cannot remove shared memory\n");
+    exit(1);
+  } 
+  pthread_join(threads[0], NULL);
+  pthread_join(threads[1], NULL);
+  pthread_join(threads[2], NULL);
+  pthread_join(threads[3], NULL);
+  write_ppm("buddha-mandelbrot-480.ppm", pixels,size, size);
   // compute image
+  free(pixels);
+  free(palette);
+  return 0;
 }
+
--- A09/thread_mandelbrot.c
+++ A09/thread_mandelbrot.c
@@ -9,7 +9,52 @@
 #include <sys/wait.h>
 #include <sys/shm.h>
 #include <sys/ipc.h>
-
+#include <pthread.h>
+struct thread_data {
+  struct ppm_pixel* pixels; 
+  struct ppm_pixel* palette;
+  int sizex;
+  int sizey; 
+  int startx;
+  int starty; 
+  int xmin; 
+  int ymin; 
+  int xmax; 
+  int ymax; 
+  int maxIterations;
+};
+void *calc(void *userdata){
+  struct thread_data *data = (struct thread_data *) userdata;
+  for(int i = data->startx; i < data->sizex; i++){
+    for(int j = data->starty; j < data->sizey; j++){
+      float xfrac = i / (float)data->sizex;
+      float yfrac = j / (float)data->sizey;
+      float x0 = data->xmin + xfrac * (data->xmax - data->xmin);
+      float y0 = data->ymin + yfrac * (data->ymax - data->ymin);
+      float x = 0;
+      float y = 0;
+      int iter = 0;
+      while (iter < data->maxIterations && x*x + y*y < 2*2){
+        float xtmp = x*x - y*y + x0;
+        y = 2*x*y + y0;
+        x = xtmp;
+        iter++;
+      }
+      if (iter < data->maxIterations){
+        data->pixels[j * data->sizey + i].red = data->palette[iter].red;
+        data->pixels[j * data->sizey + i].green = data->palette[iter].green;
+        data->pixels[j * data->sizey + i].blue = data->palette[iter].blue;
+      } // escaped
+      else{
+        data->pixels[j * data->sizex + i].red = 0;
+        data->pixels[j * data->sizex + i].green = 0;
+        data->pixels[j * data->sizex + i].blue = 0;
+      }
+        
+    }
+  }
+  return NULL;
+}
 int main(int argc, char* argv[]) {
   struct timeval stop, start;
   double timer = time(0);
@@ -60,167 +105,199 @@ int main(int argc, char* argv[]) {
     perror("Error: cannot access shared memory\n");
     exit(1);
   } 
-
-  buffer[40] = '\0';
+  pthread_t threads[4];
+  struct thread_data data[4];
   for (int i = 0; i < 4; i++) {
-    int pid = fork();
-    if (pid == 0) {
-      for(int i = 0; i < size / 2; i++){
-    for(int j = 0; j < size / 2; j++){
-      float xfrac = i / (float)size;
-      float yfrac = j / (float)size;
-      float x0 = xmin + xfrac * (xmax - xmin);
-      float y0 = ymin + yfrac * (ymax - ymin);
-      float x = 0;
-      float y = 0;
-      int iter = 0;
-      while (iter < maxIterations && x*x + y*y < 2*2){
-        float xtmp = x*x - y*y + x0;
-        y = 2*x*y + y0;
-        x = xtmp;
-        iter++;
-      }
-      if (iter < maxIterations){
-        pixels[j * size + i].red = palette[iter].red;
-        pixels[j * size + i].green = palette[iter].green;
-        pixels[j * size + i].blue = palette[iter].blue;
-      } // escaped
-      else{
-        pixels[j * size + i].red = 0;
-        pixels[j * size + i].green = 0;
-        pixels[j * size + i].blue = 0;
-      }
-        
-    }
-  }
-      exit(0);
-    } else {
-      printf("Launched child process: %d\n", pid);
-    }
-  }
-
-  for (int i = 0; i < 4; i++) {
-    int status;
-    int pid = wait(&status);
+    data[i].maxIterations = maxIterations;
+    data[i].pixels = pixels;
+    data[i].palette = palette;
+    data[i].xmax = xmax;
+    data[i].xmin = xmin;
+    data[i].ymax = ymax;
+    data[i].ymin = ymin;
     if(i == 0){
-      for(int i = size / 2; i < size; i++){
-    for(int j = size / 2; j < size; j++){
-      float xfrac = i / (float)size;
-      float yfrac = j / (float)size;
-      float x0 = xmin + xfrac * (xmax - xmin);
-      float y0 = ymin + yfrac * (ymax - ymin);
-      float x = 0;
-      float y = 0;
-      int iter = 0;
-      while (iter < maxIterations && x*x + y*y < 2*2){
-        float xtmp = x*x - y*y + x0;
-        y = 2*x*y + y0;
-        x = xtmp;
-        iter++;
-      }
-      if (iter < maxIterations){
-        pixels[j * size + i].red = palette[iter].red;
-        pixels[j * size + i].green = palette[iter].green;
-        pixels[j * size + i].blue = palette[iter].blue;
-      } // escaped
-      else{
-        pixels[j * size + i].red = 0;
-        pixels[j * size + i].green = 0;
-        pixels[j * size + i].blue = 0;
-      }
-        
-    }
-  }
-    } else if (i == 1){
-      for(int i = 0; i < size / 2; i++){
-    for(int j = size / 2; j < size; j++){
-      float xfrac = i / (float)size;
-      float yfrac = j / (float)size;
-      float x0 = xmin + xfrac * (xmax - xmin);
-      float y0 = ymin + yfrac * (ymax - ymin);
-      float x = 0;
-      float y = 0;
-      int iter = 0;
-      while (iter < maxIterations && x*x + y*y < 2*2){
-        float xtmp = x*x - y*y + x0;
-        y = 2*x*y + y0;
-        x = xtmp;
-        iter++;
-      }
-      if (iter < maxIterations){
-        pixels[j * size + i].red = palette[iter].red;
-        pixels[j * size + i].green = palette[iter].green;
-        pixels[j * size + i].blue = palette[iter].blue;
-      } // escaped
-      else{
-        pixels[j * size + i].red = 0;
-        pixels[j * size + i].green = 0;
-        pixels[j * size + i].blue = 0;
-      }
-        
-    }
-  }
+      data[i].startx = 0;
+      data[i].starty = 0;
+      data[i].sizex = size/2;
+      data[i].sizey = size / 2;
+    } else if(i == 1){
+      data[i].startx = 0;
+      data[i].starty = size / 2;
+      data[i].sizex = size / 2;
+      data[i].sizey = size;
     } else if(i == 2){
-      for(int i = size / 2; i < size; i++){
-    for(int j = 0; j < size / 2; j++){
-      float xfrac = i / (float)size;
-      float yfrac = j / (float)size;
-      float x0 = xmin + xfrac * (xmax - xmin);
-      float y0 = ymin + yfrac * (ymax - ymin);
-      float x = 0;
-      float y = 0;
-      int iter = 0;
-      while (iter < maxIterations && x*x + y*y < 2*2){
-        float xtmp = x*x - y*y + x0;
-        y = 2*x*y + y0;
-        x = xtmp;
-        iter++;
-      }
-      if (iter < maxIterations){
-        pixels[j * size + i].red = palette[iter].red;
-        pixels[j * size + i].green = palette[iter].green;
-        pixels[j * size + i].blue = palette[iter].blue;
-      } // escaped
-      else{
-        pixels[j * size + i].red = 0;
-        pixels[j * size + i].green = 0;
-        pixels[j * size + i].blue = 0;
-      }
-        
-    }
-  }
+      data[i].startx = size/2;
+      data[i].sizex = size;
+      data[i].starty = 0;
+      data[i].sizey = size /2;
     } else {
-      for(int i = 0; i < size / 2; i++){
-    for(int j = 0; j < size / 2; j++){
-      float xfrac = i / (float)size;
-      float yfrac = j / (float)size;
-      float x0 = xmin + xfrac * (xmax - xmin);
-      float y0 = ymin + yfrac * (ymax - ymin);
-      float x = 0;
-      float y = 0;
-      int iter = 0;
-      while (iter < maxIterations && x*x + y*y < 2*2){
-        float xtmp = x*x - y*y + x0;
-        y = 2*x*y + y0;
-        x = xtmp;
-        iter++;
-      }
-      if (iter < maxIterations){
-        pixels[j * size + i].red = palette[iter].red;
-        pixels[j * size + i].green = palette[iter].green;
-        pixels[j * size + i].blue = palette[iter].blue;
-      } // escaped
-      else{
-        pixels[j * size + i].red = 0;
-        pixels[j * size + i].green = 0;
-        pixels[j * size + i].blue = 0;
-      }
+      data[i].startx = size/2;
+      data[i].sizex = size;
+      data[i].starty = size/2;
+      data[i].sizey = size;
+    }
+    pthread_create(&threads[i], NULL, calc, (void*) &data[i]);
+  }
+  //buffer[40] = '\0';
+  // for (int i = 0; i < 4; i++) {
+  //   int pid = fork();
+  //   if (pid == 0) {
+  //     for(int i = 0; i < size / 2; i++){
+  //   for(int j = 0; j < size / 2; j++){
+  //     float xfrac = i / (float)size;
+  //     float yfrac = j / (float)size;
+  //     float x0 = xmin + xfrac * (xmax - xmin);
+  //     float y0 = ymin + yfrac * (ymax - ymin);
+  //     float x = 0;
+  //     float y = 0;
+  //     int iter = 0;
+  //     while (iter < maxIterations && x*x + y*y < 2*2){
+  //       float xtmp = x*x - y*y + x0;
+  //       y = 2*x*y + y0;
+  //       x = xtmp;
+  //       iter++;
+  //     }
+  //     if (iter < maxIterations){
+  //       pixels[j * size + i].red = palette[iter].red;
+  //       pixels[j * size + i].green = palette[iter].green;
+  //       pixels[j * size + i].blue = palette[iter].blue;
+  //     } // escaped
+  //     else{
+  //       pixels[j * size + i].red = 0;
+  //       pixels[j * size + i].green = 0;
+  //       pixels[j * size + i].blue = 0;
+  //     }
         
-    }
-  }
-    }
-    printf("Child process complete: %d\n", pid);
-  }
+  //   }
+  // }
+  //     exit(0);
+  //   } else {
+  //     printf("Launched child process: %d\n", pid);
+  //   }
+  // }
+
+  // for (int i = 0; i < 4; i++) {
+  //   int status;
+  //   int pid = wait(&status);
+  //   if(i == 0){
+  //     for(int i = size / 2; i < size; i++){
+  //   for(int j = size / 2; j < size; j++){
+  //     float xfrac = i / (float)size;
+  //     float yfrac = j / (float)size;
+  //     float x0 = xmin + xfrac * (xmax - xmin);
+  //     float y0 = ymin + yfrac * (ymax - ymin);
+  //     float x = 0;
+  //     float y = 0;
+  //     int iter = 0;
+  //     while (iter < maxIterations && x*x + y*y < 2*2){
+  //       float xtmp = x*x - y*y + x0;
+  //       y = 2*x*y + y0;
+  //       x = xtmp;
+  //       iter++;
+  //     }
+  //     if (iter < maxIterations){
+  //       pixels[j * size + i].red = palette[iter].red;
+  //       pixels[j * size + i].green = palette[iter].green;
+  //       pixels[j * size + i].blue = palette[iter].blue;
+  //     } // escaped
+  //     else{
+  //       pixels[j * size + i].red = 0;
+  //       pixels[j * size + i].green = 0;
+  //       pixels[j * size + i].blue = 0;
+  //     }
+        
+  //   }
+  // }
+  //   } else if (i == 1){
+  //     for(int i = 0; i < size / 2; i++){
+  //   for(int j = size / 2; j < size; j++){
+  //     float xfrac = i / (float)size;
+  //     float yfrac = j / (float)size;
+  //     float x0 = xmin + xfrac * (xmax - xmin);
+  //     float y0 = ymin + yfrac * (ymax - ymin);
+  //     float x = 0;
+  //     float y = 0;
+  //     int iter = 0;
+  //     while (iter < maxIterations && x*x + y*y < 2*2){
+  //       float xtmp = x*x - y*y + x0;
+  //       y = 2*x*y + y0;
+  //       x = xtmp;
+  //       iter++;
+  //     }
+  //     if (iter < maxIterations){
+  //       pixels[j * size + i].red = palette[iter].red;
+  //       pixels[j * size + i].green = palette[iter].green;
+  //       pixels[j * size + i].blue = palette[iter].blue;
+  //     } // escaped
+  //     else{
+  //       pixels[j * size + i].red = 0;
+  //       pixels[j * size + i].green = 0;
+  //       pixels[j * size + i].blue = 0;
+  //     }
+        
+  //   }
+  // }
+  //   } else if(i == 2){
+  //     for(int i = size / 2; i < size; i++){
+  //   for(int j = 0; j < size / 2; j++){
+  //     float xfrac = i / (float)size;
+  //     float yfrac = j / (float)size;
+  //     float x0 = xmin + xfrac * (xmax - xmin);
+  //     float y0 = ymin + yfrac * (ymax - ymin);
+  //     float x = 0;
+  //     float y = 0;
+  //     int iter = 0;
+  //     while (iter < maxIterations && x*x + y*y < 2*2){
+  //       float xtmp = x*x - y*y + x0;
+  //       y = 2*x*y + y0;
+  //       x = xtmp;
+  //       iter++;
+  //     }
+  //     if (iter < maxIterations){
+  //       pixels[j * size + i].red = palette[iter].red;
+  //       pixels[j * size + i].green = palette[iter].green;
+  //       pixels[j * size + i].blue = palette[iter].blue;
+  //     } // escaped
+  //     else{
+  //       pixels[j * size + i].red = 0;
+  //       pixels[j * size + i].green = 0;
+  //       pixels[j * size + i].blue = 0;
+  //     }
+        
+  //   }
+  // }
+  //   } else {
+  //     for(int i = 0; i < size / 2; i++){
+  //   for(int j = 0; j < size / 2; j++){
+  //     float xfrac = i / (float)size;
+  //     float yfrac = j / (float)size;
+  //     float x0 = xmin + xfrac * (xmax - xmin);
+  //     float y0 = ymin + yfrac * (ymax - ymin);
+  //     float x = 0;
+  //     float y = 0;
+  //     int iter = 0;
+  //     while (iter < maxIterations && x*x + y*y < 2*2){
+  //       float xtmp = x*x - y*y + x0;
+  //       y = 2*x*y + y0;
+  //       x = xtmp;
+  //       iter++;
+  //     }
+  //     if (iter < maxIterations){
+  //       pixels[j * size + i].red = palette[iter].red;
+  //       pixels[j * size + i].green = palette[iter].green;
+  //       pixels[j * size + i].blue = palette[iter].blue;
+  //     } // escaped
+  //     else{
+  //       pixels[j * size + i].red = 0;
+  //       pixels[j * size + i].green = 0;
+  //       pixels[j * size + i].blue = 0;
+  //     }
+        
+  //   }
+  // }
+  //   }
+  //   printf("Child process complete: %d\n", pid);
+  //}
   gettimeofday(&stop, NULL);
   timer = stop.tv_sec - start.tv_sec + (stop.tv_usec - start.tv_usec)/1.e6;
   printf("time is %f\n",timer);
@@ -234,9 +311,10 @@ int main(int argc, char* argv[]) {
     perror("Error: cannot remove shared memory\n");
     exit(1);
   }   
-  write_ppm("multi-mandelbrot-100.ppm", pixels,size, size);
+  write_ppm("mandelbrot-100.ppm", pixels,size, size);
   // compute image
   free(pixels);
   free(palette);
   return 0;
 }
+
